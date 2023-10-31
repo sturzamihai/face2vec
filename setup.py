0b8cabe6b67fd4a4eb4e0fcb01267b5cc7e5c8cd
Mihai-George Sturza <mihai.sturza@nevexo.ro>
2023-10-31 09:42:31 +0200

    Added setup files

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='face2vec',
+    version='0.1',
+    license='MIT',
+    author='Mihai-George Sturza',
+    author_email='contact@mihaisturza.ro',
+    description='A Python package for transforming faces into feature vectors.',
+    url='https://github.com/sturzamihai/face2vec',
+)
